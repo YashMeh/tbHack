350f059e901f7d798ee2a2a9cb853e5b0c11563e
Yash <mehrotra.yash9@gmail.com>
2018-12-10 08:57:54 -0800

    Fixed port bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express=require("express"),
     bodyParser=require("body-parser"),
-    port=process.env.port || 3000,
+    port=process.env.PORT || 3000,
     mongoose=require("mongoose"),
     app=express()
 //mongoose.connect("mongodb://localhost/emailer");	
@@ -9,6 +9,9 @@ mongoose.connect("mongodb://Yash123:yash1234@ds039768.mlab.com:39768/tbhack");
 app.use(bodyParser.urlencoded({extended:true}));
 var apiroutes=require("./routes/apiRoutes");
 app.use("/api/user",apiroutes);
+app.get("/",function(req,res){
+    res.send("The api route is at /api/user");
+})
 app.listen(port,function(err){
     if(err)
     {
