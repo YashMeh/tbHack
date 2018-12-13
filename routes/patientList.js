c559caa1fc5f492da103e5ee644ab53f40d4f9bd
Yash <mehrotra.yash9@gmail.com>
2018-12-13 10:41:27 -0800

    Made distances promises

--- routes/patientList.js
+++ routes/patientList.js
@@ -5,6 +5,7 @@ var request=require("request"),
 var gs=[];
 function distanceBetween(lat1,lon1,lat2,lon2)
 {
+    
     var R = 6371e3; // metres
     var φ1 = lat1.toRadians();
     var φ2 = lat2.toRadians();
@@ -17,58 +18,115 @@ function distanceBetween(lat1,lon1,lat2,lon2)
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
     var d = R * c;
-    return d;
+    return Math.abs(d);
+    
+    
 }
 function giveLatLon(pincode)
 {
     var url="https://maps.googleapis.com/maps/api/geocode/json?address="+pincode+"&key=AIzaSyAvEpdyB6qT4qmaU_qkkx8ziwX_8kAOEp0";
-    request(url,function(err,resp,userDet){
-        if(!err && resp.statusCode==200)
-        {
-         var La;
-         var Lo;
-         var parsedResp=JSON.parse(userDet);
-         La=parsedResp.results[0].geometry.bounds.northeast.lat;
-         Lo=parsedResp.results[0].geometry.bounds.northeast.lng;
-         var ob={"latitude":La,"longitude":Lo};
-         return ob;
-        }    
-
-})
-}
-function giveUserList(pincode,dist)
-{
-    var url="https://maps.googleapis.com/maps/api/geocode/json?address="+pincode+"&key=AIzaSyAvEpdyB6qT4qmaU_qkkx8ziwX_8kAOEp0";
-    request(url,function(err,resp,docDet){
-        if(!err && resp.statusCode==200)
-        {
-         var docLa;
-         var docLo;
-         var parsedResp=JSON.parse(docDet);
-         docLa=parsedResp.results[0].geometry.bounds.northeast.lat;
-         docLo=parsedResp.results[0].geometry.bounds.northeast.lng;
-         var userDataUrl="https://tbhack2.herokuapp.com/api/user";
-         request(userDataUrl,function(userErr,userResp,userDet){
-             if(!err && resp.statusCode==200)
-             {
-                 var parsedUserData=JSON.parse(userDet);
-                 parsedUserData.forEach(function(user){
-                     var recObj=giveLatLon(user.pc);
-                     var lat1=recObj.latitude;
-                     var lon1=recObj.longitude;
-                     var distance=distanceBetween(lat1,lon1,docLa,docLo);
-                     if(distance<dist)
-                     gs.push(user);   
-                 })
-             }
-         })   
-        }
+    return new Promise(function(resolve,reject){
+        request(url,function(err,resp,userDet){
+            if(!err && resp.statusCode==200)
+            {
+             var La;
+             var Lo;
+             var parsedResp=JSON.parse(userDet);
+             La=parsedResp.results[0].geometry.bounds.northeast.lat;
+             Lo=parsedResp.results[0].geometry.bounds.northeast.lng;
+             var ob={"latitude":La,"longitude":Lo};
+             resolve(ob);
+            }  
+            else
+            reject(err);  
+    
     })
-    return gs;
+    })
+    
+}
+function giveUserDetails()
+{   
+    var url="https://tbhack2.herokuapp.com/api/user";
+    return new Promise(function(resolve,reject){
+        request(url,function(err,resp,userDet){
+            if(!err && resp.statusCode==200)
+            {
+             var ob=JSON.parse(userDet);   
+             resolve(ob);
+            }    
+            else
+            reject(err);
+    })
+    })
+    
+}
+function giveDocDetails(docId)
+{
+    var url="https://tbhack2.herokuapp.com/api/doc/"+docId;
+    return new Promise(function(resolve,reject){
+        request(url,function(err,resp,docDet){
+            if(!err && resp.statusCode==200)
+            {
+                var ob=JSON.parse(docDet);
+                resolve(ob);
+            }
+            else
+            reject(err);
+        })
+    })
 }
 
 
-router.get("/",function(req,res){
-console.log(giveUserList('211003',28));
+router.get("/:id",function(req,res){
+    var rs;
+    var docLa;
+    var docLo;
+    var docPin;
+    var docId=req.params.id;
+    giveDocDetails(docId).then(function(doc){
+        docPin=doc.pc;
+    }).then(function(e){
+        giveLatLon(docPin).then(function(docDet){
+            docLa=docDet.latitude;
+            docLo=docDet.longitude;
+            console.log("docDet: "+docDet.latitude);
+        }).then(function(ee){
+            giveUserDetails().then(function(userList){
+                userList.forEach(function(user){
+                    var userPin=user.pc;
+                    var userLa;
+                    var userLo;
+                    giveLatLon(userPin).then(function(userDet){
+                        userLa=userDet.latitude;
+                        userLo=userDet.longitude;
+                        console.log(userLa,userLo,docLa,docLo);
+                        var R = 6371e3; // metres
+                        var pi=Math.PI;
+                        var φ1 = userLa*(pi/180);
+                        var φ2 = docLa*(pi/180);
+                        var Δφ = Math.abs((docLa-userLa))*(pi/180);
+                        var Δλ = Math.abs((userLo-docLo))*(pi/180);
+
+                        var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                                Math.cos(φ1) * Math.cos(φ2) *
+                                Math.sin(Δλ/2) * Math.sin(Δλ/2);
+                        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+                        var d = (R * c)/1000;
+                        console.log("distance is :"+d);
+                        if(d<10)
+                        gs.push(user);
+                        
+                    })
+                    
+                    
+                })
+                
+            })
+            
+        })
+    })
+    
+   
 })    
 module.exports=router    
