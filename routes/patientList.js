38e6a841cdc7c9859080d77fcd104d31e4ff129e
Yash <mehrotra.yash9@gmail.com>
2018-12-15 19:52:27 -0800

    Added parient Search Route

--- routes/patientList.js
+++ routes/patientList.js
@@ -3,6 +3,24 @@ var request=require("request"),
     router=express.Router()
 
 var gs=[];
+function distanceBetween(lat1,lon1,lat2,lon2)
+{
+    var R = 6371e3; // metres
+    var pi=Math.PI;
+    
+    var φ1 = lat1*(pi/180);
+    var φ2 = lat2*(pi/180);
+    var Δφ = (lat2-lat1)*(pi/180);
+    var Δλ = (lon2-lon1)*(pi/180);
+    
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    
+    var d = (R * c)/1000;
+return d;
+}
 function routerFunction(docId)
 {
     var url="https://tbhack2.herokuapp.com/api/doc/"+docId;
@@ -32,8 +50,9 @@ function routerFunction(docId)
                     {
                         var userL=JSON.parse(body3);
                         userList=userL;
-                        for(var i=0;i<userList.length;i++){
-                            var userPc=userList[i].pincode;
+                        userList.forEach(function(user){
+                            var userPc=user.pincode;
+                            //var currUser=userList[i];
                             var url4="https://maps.googleapis.com/maps/api/geocode/json?address="+userPc+"&key=AIzaSyAvEpdyB6qT4qmaU_qkkx8ziwX_8kAOEp0";
                             request(url4,function(err4,response4,body4){
                                 if(!err4 && response4.statusCode==200)
@@ -41,13 +60,18 @@ function routerFunction(docId)
                                     var userDet=JSON.parse(body4);
                                     var userLa=userDet.results[0].geometry.bounds.northeast.lat;
                                     var userLo=userDet.results[0].geometry.bounds.northeast.lng;
-                                    console.log(userLa);
-                                    gs.push({docLa,docLa});
-                                    console.log("reached !!");
+                                    var dist=distanceBetween(docLa,docLo,userLa,userLo);
+                                    console.log("User Coordinates "+userLa,userLo);
+                                    console.log("Doctor Coordinates "+docLa,docLo);
+                                    console.log("distance "+dist);
+                                    if(dist<100)
+                                    {
+                                        gs.push(user);
+                                    }
                                 }
                             })    
                            
-                        };
+                        });
                         
                     }
                     setTimeout(function(e){
@@ -65,6 +89,7 @@ router.get("/:id",function(req,res){
     var ID=req.params.id;
     routerFunction(ID).then(function(e){
         console.log(e);
+        res.json(e);
     })
 })
 module.exports=router    
