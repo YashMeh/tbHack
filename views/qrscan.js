2b20e93577eba6a944e1e626ebea008a6043f43f
Aditya Chandak <adityachandak287@gmail.com>
2018-12-16 10:45:07 +0530

    Added city,state and dob and removed age

--- views/qrscan.js
+++ views/qrscan.js
@@ -1,6 +1,7 @@
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview'),backgroundScan: false});
 let x;
 let data = {};
+
 scanner.addListener('scan', function (content) {
     parser = new DOMParser();
     xmlDoc = parser.parseFromString(content,"text/xml");
@@ -9,27 +10,35 @@ scanner.addListener('scan', function (content) {
     for(let i=0;i<x.length;i++) {
         data[x[i].nodeName] = x[i].nodeValue;
     }
-    let address = [data["house"],data["street"],data["lm"],data["loc"],data["vtc"],data["state"]].join(", ");
+    let address = [data["house"],data["street"],data["lm"],data["loc"]].join(", ");
+    let city = data["vtc"];
+    let state = data["state"];
     let aadhar = data["uid"];
     let name = data["name"];
-    let dob = new Date(data["dob"]);
-    let today = new Date();
-    let age = today.getFullYear() - dob.getFullYear();
-    let mdiff = today.getMonth() - dob.getMonth();
-    if (mdiff < 0 || (mdiff === 0 && today.getDate() < dob.getDate())) {
-        age--;
-    }
+    let dob = data["dob"];
+    //let today = new Date();
+    // let age = today.getFullYear() - dob.getFullYear();
+    // let mdiff = today.getMonth() - dob.getMonth();
+    // if (mdiff < 0 || (mdiff === 0 && today.getDate() < dob.getDate())) {
+    //     age--;
+    // }
     let pincode = data["pc"];
     let gender = (data["gender"] === "M")?"Male":"Female";
 
     document.getElementById('aadhar').value = aadhar;
     document.getElementById('name').value = name;
-    document.getElementById('age').value = age;
+    document.getElementById('dob').value = dob;
     document.getElementById('gender').value = gender;
     document.getElementById('address').value= address;
+    document.getElementById('city').value = city;
+    document.getElementById('state').value = state;
     document.getElementById('pincode').value = pincode;
     //output = "Aadhar Nummber: " + aadhar + "\nName: "+ name + "\nAge: " + age +"\nGender: "+ gender +"\nAddress: " + address + "\nPincode: " + pincode;
     //console.log(data);
+    scanner.stop().then(() =>   {
+        $("#unscanned").hide();
+        $("#scanned").show();
+    });
 });
 Instascan.Camera.getCameras().then(function (cameras) {
     if (cameras.length > 0) {
@@ -46,23 +55,35 @@ Instascan.Camera.getCameras().then(function (cameras) {
 
 document.getElementById("submit").addEventListener("click", sendData);
 function sendData() {
+    $(".container").hide();
+    $("#success").hide();
+    $("#loading").show();
     let patient = {};
     patient["aadhar"] = document.getElementById('aadhar').value;
     patient["name"] = document.getElementById('name').value;
-    patient["age"] = document.getElementById('age').value;
+    patient["dob"] = document.getElementById('dob').value;
     patient["gender"] = document.getElementById('gender').value;
     patient["address"] = document.getElementById('address').value;
+    patient["city"] = document.getElementById('city').value;
+    patient["state"] = document.getElementById('state').value;
     patient["pincode"] = document.getElementById('pincode').value;
 
     let symptoms = document.getElementsByName("symptoms");
     for(let i=0;i<symptoms.length;i++)    {
         patient[symptoms[i].value] = symptoms[i].checked;
     }
-
+    console.log(patient);
     const url = "https://tbhack2.herokuapp.com/api/user";
     $.post(url,patient,function(data, status)   {
+        console.log(data+" and status is "+status);
+        console.log(data);
+        console.log(status);
+        if(status === "success") {
+            $(".container").hide();
+            $("#success").show();
+            $("#loading").hide();
+        }
     });
-
 }
 
 
