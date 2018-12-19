5ec213d59fb9e8ad59cacc11d7a38e4530071edc
Aditya Chandak <adityachandak287@gmail.com>
2018-12-19 15:28:03 +0530

    final

--- views/qrscan.js
+++ views/qrscan.js
@@ -1,3 +1,9 @@
+$(".next").click(function(){
+    $(".cont1").fadeOut(1000,function(){
+        $(".cont2").fadeIn(1000);
+    });
+});
+
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview'),backgroundScan: false});
 let x;
 let data = {};
@@ -5,7 +11,6 @@ scanner.addListener('scan', function (content) {
     parser = new DOMParser();
     xmlDoc = parser.parseFromString(content,"text/xml");
     x = Array.from(xmlDoc.all[0].attributes);
-    let output = "";
     for(let i=0;i<x.length;i++) {
         data[x[i].nodeName] = x[i].nodeValue;
     }
@@ -40,6 +45,8 @@ scanner.addListener('scan', function (content) {
         $("#preview").hide();
     });
 });
+
+
 Instascan.Camera.getCameras().then(function (cameras) {
     if (cameras.length > 0) {
         if(cameras.length > 1)
@@ -55,52 +62,54 @@ Instascan.Camera.getCameras().then(function (cameras) {
 
 document.getElementById("submit").addEventListener("click", sendData);
 function sendData() {
-    $(".container").hide();
+    $(".cont2").hide();
     $("#success").hide();
     $("#loading").show();
     let patient = {};
     patient["aadhar"] = document.getElementById('aadhar').value;
-    patient["name"] = document.getElementById('name').value;
-    patient["dob"] = document.getElementById('dob').value;
-    patient["gender"] = document.getElementById('gender').value;
-    patient["address"] = document.getElementById('address').value;
+    //patient["name"] = document.getElementById('name').value;
+    //patient["dob"] = document.getElementById('dob').value;
+    //patient["gender"] = document.getElementById('gender').value;
+    //patient["address"] = document.getElementById('address').value;
     patient["city"] = document.getElementById('city').value;
     patient["state"] = document.getElementById('state').value;
-    patient["pincode"] = document.getElementById('pincode').value;
+    //patient["pincode"] = document.getElementById('pincode').value;
 
     let symptoms = document.getElementsByName("symptoms");
     patient["symptoms"] = [];
+    patient["symptomCount"] = 0;
     for(let i=0;i<symptoms.length;i++)    {
         patient["symptoms"][i] = (symptoms[i].checked)?1:0;
+        if(symptoms[i].checked) {
+            patient["symptomCount"]++;
+        }
     }
-
-    let meds = document.getElementById("medicine").options;
-    let medicine;
-    for(let i=1; i<meds.length;i++) {
-        if (meds[i].selected === true)
-            medicine = meds[i].value;
-    }
-    medicine = medicine.split("");
-    for(let i=0;i<medicine.length;i++)  {
-        medicine[i] = parseInt(medicine[i]);
-    }
-    patient["medicine"] = medicine;
-    patient["state"] = false;
+    // let meds = document.getElementById("medicine").options;
+    // let medicine;
+    // for(let i=1; i<meds.length;i++) {
+    //     if (meds[i].selected === true)
+    //         medicine = meds[i].value;
+    // }
+    // medicine = medicine.split("");
+    // for(let i=0;i<medicine.length;i++)  {
+    //     medicine[i] = parseInt(medicine[i]);
+    // }
+    // patient["medicine"] = medicine;
+    patient["tbstatus"] = 0;
     patient["date"] = Date.now();
+    patient["medicine"] = ["0","0","0","0"];
+
     console.log(patient);
-    const url = "https://tbhack2.herokuapp.com/api/user";
+    const url = "https://tbhack2.herokuapp.com/log";
     $.post(url,patient,function(data, status)   {
         console.log(data+" and status is "+status);
         console.log(data);
         console.log(status);
         if(status === "success") {
-            $(".container").hide();
+            // $(".container").hide();
+            $(".cont2").hide();
             $("#success").show();
             $("#loading").hide();
         }
     });
 }
-
-
-
-
